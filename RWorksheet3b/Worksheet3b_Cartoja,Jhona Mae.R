6ea5122d4ad6f47410198cdc100798dfefc67ace
iamjhona <jhonamae.cartoja@students.isatu.edu.ph>
2022-11-22 13:52:06 +0700

    2nd commit

---
+++ RWorksheet3b/Worksheet3b_Cartoja,Jhona Mae.R
@@ -0,0 +1,74 @@
+#1. Create a data frame using the table below.
+#a. Write the codes.
+
+Respondents <- c(seq(1,20))
+Sex <- c(2,2,1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,2)
+FathersOccupation <- c(1,3,3,3,1,2,3,1,1,1,3,2,1,3,3,1,3,1,2,1)
+Personsathome <- c(5,7,3,8,5,9,6,7,8,4,7,5,4,7,8,8,3,11,7,6) 
+Siblingsatschool <- c(6,4,4,1,2,1,5,3,1,2,3,2,5,5,2,1,2,5,3,2)
+Typesofhouses <- c(1,2,3,1,1,3,3,1,2,3,2,3,2,2,3,3,3,3,3,2)
+
+dataframe <- data.frame(Respondents,Sex,FathersOccupation,Personsathome,
+                     Siblingsatschool,Typesofhouses)
+
+
+#b.Describe the data. Get the structure or the summary of the data
+
+summary(dataframe)
+
+
+
+#c. Is the mean number of siblings attending is 5?
+
+Ans: No
+
+
+#d. Extract the 1st two rows and then all the columns using the subsetting functions.
+#Write the codes and its output.
+
+A1 <- subset(dataframe[1:2, 1:6, drop = FALSE])
+A1
+
+
+#e. Extract 3rd and 5th row with 2nd and 4th column. Write the codes and its
+result.
+
+A2 <- subset(dataframe[c(3,5),c(2,4)])
+A2
+
+
+#f. Select the variable types of houses then store the vector that results as types_houses.
+#Write the codes.
+
+A3 <- subset(dataframe[c(1:20), c(2,6)])
+type_houses <- A3
+
+
+#g. Select only all Males respondent that their father occupation was farmer. Write
+#the codes and its output.
+
+A4 <- subset(dataframe[c(1:20), c(2,3)])
+
+Males <- A4[dataframe$FathersOccupation == '1',]
+Males
+
+
+#h. Select only all females respondent that have greater than or equal to 5 number
+#of siblings attending school. Write the codes and its outputs
+
+A5 <- subset(dataframe[c(1:20), c(2,5)])
+
+females <- A5[dataframe$Siblingsatschool == '1',]
+females
+
+
+#2. Write a R program to create an empty data frame. Using the following codes:
+dataframe = data.frame(Integers=integer(),
+                Doubles=double(), 
+                Characters=character(),
+                Logicals=logical(),
+                Factors=factor(),
+                stringsAsFactors=FALSE)
+
+                print("Structure of the empty dataframe:")
+                print(str(dataframe))
